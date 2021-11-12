d778247bf0ea0f1ff74a5d123a973a8a2aaee43e
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-11-12 16:55:15 +1100

    Initial Redux addition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { configureStore } from '@reduxjs/toolkit';
 import './App.css';
 
 function App() {
+  // State
+  const initialState = {value: 0}
+  
+  // Action creator
+  const addNewItem = value => {
+    return {
+      type: 'item/addNewItem',
+      payload: value
+    }
+  }
+
+  // Reducer
+  const itemReducer = (state = initialState) => {
+    return {
+      ...state,
+      item: state.payload
+    }
+  }
+
+  // Store
+  const store = configureStore({reducer: itemReducer})
+
+  store.dispatch(addNewItem(1))
+
+  console.log(store.getState())
+
   return (
     <h1>Hello world!</h1>
   );
