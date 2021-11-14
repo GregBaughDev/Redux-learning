a814efc88933ce56a201a2202016b16c4942c4db
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-11-14 16:15:57 +1100

    Updates to logic

--- src/App.js
+++ src/App.js
@@ -5,32 +5,51 @@ import './App.css';
 function App() {
   // State
   const initialState = {value: 0}
-  
+
   // Action creator
-  const addNewItem = value => {
-    return {
-      type: 'item/addNewItem',
-      payload: value
+  const addNewItem = (evt) => {
+    if(evt === 'Increment'){
+      return {
+        type: 'item/itemIncrement',
+      }
+    } else if (evt === 'Decrement'){
+      return {
+        type: 'item/itemDecrement'
+      }
     }
   }
 
   // Reducer
-  const itemReducer = (state = initialState) => {
-    return {
-      ...state,
-      item: state.payload
+  const itemReducer = (state = initialState, action) => {
+    if(action.type === 'item/itemIncrement'){
+      return {
+        ...state,
+        value: state.value + 1
+      }
+    } else if(action.type === 'item/itemDecrement'){
+      return {
+        ...state,
+        value: state.value - 1
+      }
     }
+    return state
   }
 
   // Store
   const store = configureStore({reducer: itemReducer})
 
-  store.dispatch(addNewItem(1))
-
-  console.log(store.getState())
+  const action = (e) => {
+    store.dispatch(addNewItem(e.target.innerText))
+    console.log(store.getState().value)
+  }
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      {/* <h2>{store.getState().value}</h2> */}
+      <button onClick={e =>action(e)}>Increment</button>
+      <button onClick={e =>action(e)}>Decrement</button>
+    </>
   );
 }
 
